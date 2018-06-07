06d8f816d8349f382635135ae3bb35db8c7bc3f2
anmolrox <34227543+anmolrox@users.noreply.github.com>
2018-06-07 23:33:00 +0530

    hi

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wer:app
---
+++ wer.py
@@ -0,0 +1,30 @@
+import os
+from flask import Flask,request,jsonify,json
+import random
+import secrets
+app=Flask(__name__)
+
+a=["i am a god","im not a god","im shit","inspirational my fucking ass","boooooooom"]
+
+
+@app.route('/')
+def index():
+
+    return 'do you want to 1)see all the quotes 2)select a random quote 3)add a new quote '
+
+@app.route('/allquotes',methods=['POST','GET'])
+def bs():
+    return jsonify(a)
+
+@app.route('/random',methods=['POST','GET'])
+def random():
+
+     return jsonify(secrets.choice(a))
+
+@app.route("/addQuote", methods = ["POST"])
+def add_quote() :
+	json_data = request.json
+
+	a.append(json_data['a'])
+
+	return "Quote added"
